2e40549d26e55b740645768a6302b5c75c148b39
pablofrers93 <pablofrers93@gmail.com>
2023-04-08 11:40:16 -0300

    Edit and Delete

--- CursoMVC/Controllers/UserController.cs
+++ CursoMVC/Controllers/UserController.cs
@@ -60,5 +60,59 @@ namespace CursoMVC.Controllers
 
             return Redirect(Url.Content("~/User/"));
         }
+
+        public ActionResult Edit(int Id) 
+        {
+            EditUserViewModel model = new EditUserViewModel();  
+
+            using (var db = new cursomvcEntities())
+            {
+                var oUser = db.user.Find(Id);
+                model.Edad = (int)oUser.edad;
+                model.Email = oUser.email;
+                model.Id = oUser.id;
+            }
+
+            return View(model);  
+        }
+
+        [HttpPost]
+        public ActionResult Edit(EditUserViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (var db = new cursomvcEntities())
+            {
+                var oUser = db.user.Find(model.Id);
+                oUser.email = model.Email;
+                oUser.edad = model.Edad;
+
+                if (model.Password != null && model.Password.Trim() != "")
+                {
+                    oUser.password = model.Password;
+                }
+
+                db.Entry(oUser).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+            }
+            return Redirect(Url.Content("~/User/"));
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int Id)
+        {
+            using (var db = new cursomvcEntities())
+            {
+                var oUser = db.user.Find(Id);
+                oUser.idState =3; //eliminaremos  
+
+                db.Entry(oUser).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+            }
+            return Content("1");
+        }
     }
 }
--- CursoMVC/Models/ViewModels/UserViewModel.cs
+++ CursoMVC/Models/ViewModels/UserViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
+using System.Web.Profile;
 
 namespace CursoMVC.Models.ViewModels
 {
@@ -21,6 +22,28 @@ namespace CursoMVC.Models.ViewModels
         [Display(Name ="Confirmar contraseña")]
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage ="Las contraseñas no son iguales")]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        public int Edad { get; set; }
+    }
+
+    public class EditUserViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(100, ErrorMessage = "El {0} debe tener al menos {1} caracteres", MinimumLength = 1)]
+        [Display(Name = "Correo Electrónico")]
+        public string Email { get; set; }
+        
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirmar contraseña")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Las contraseñas no son iguales")]
         public string ConfirmPassword { get; set; }
         [Required]
         public int Edad { get; set; }
