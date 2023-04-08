29fa55e22a2d46fe9fa07c45de89b788e948af27
pablofrers93 <pablofrers93@gmail.com>
2023-04-08 16:47:38 -0300

    Files upload

---
+++ CursoMVC/Controllers/FileController.cs
@@ -0,0 +1,42 @@
+﻿using CursoMVC.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CursoMVC.Controllers
+{
+    public class FileController : Controller
+    {
+        // GET: File
+        public ActionResult Index()
+        {
+            if (TempData["Message"]!=null)
+            {
+                ViewBag.Message = TempData["Message"].ToString();  
+            }
+
+            return View();
+        }
+
+        public ActionResult Save(FileViewModel model)       
+        {
+            string RutaSitio = Server.MapPath("~/");
+            string PathFile1 = Path.Combine(RutaSitio + "/Files/file1.png");
+            string PathFile2 = Path.Combine(RutaSitio + "/Files/file2.png");
+
+            if (!ModelState.IsValid)
+            {
+                return View("Index", model);
+            }
+
+            model.File1.SaveAs(PathFile1);
+            model.File2.SaveAs(PathFile2);
+
+            @TempData["Message"] = "Se cargaron los archivos";
+            return RedirectToAction("Index"); 
+        }
+    }
+}
---
+++ CursoMVC/Models/ViewModels/FileViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CursoMVC.Models.ViewModels
+{
+    public class FileViewModel
+    {
+        [Required]
+        [DisplayName("Mi archivo")]
+        public HttpPostedFileBase File1 { get; set; }
+        [Required]
+        [DisplayName("Mi archivo 2")]
+        public HttpPostedFileBase File2 { get; set; }
+        [Required]
+        [DisplayName("Mi cadena")]
+        public string Cadena { get; set; }
+    }
+}
